5e538ce801d9621ea68a1c2060c981d10a199344
vita <vitezslavo@gmail.com>
2013-10-18 10:36:28 +0100

    pico

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestProject::Application.config.secret_key_base = '54e4b8bebc56c6dd8bf9d028f9591fc793c4671a9e38a7af365d39e3151a6b3a6a8bc0ca7a456cdb609abd93855fde362c2f9d5b15e034c217399edbf39c6c8d'
